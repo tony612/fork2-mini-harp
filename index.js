f7376deaf52b0c6d6567cd493f40fc47506a3a9c
Tony Han <h.bing612@gmail.com>
2014-04-01 15:58:37 +0800

    add a current-time middleware

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ module.exports = function() {
   var connect = require('connect');
   var app = connect();
 
+  app.use(function(request, response, next) {
+    var url = request.url.split("/");
+
+    if (url[1] == "current-time") {
+      response.end((new Date()).toISOString() + "\n");
+    } else {
+      next();
+    }
+  });
+
   return app;
 };
